720ff81a7b5429d15f6351a17ae0b10b1b060fe1
Changhui Xu <changhuixu@hotmail.com>
2021-08-30 16:54:13 -0500

    add csharp list extensions

---
+++ CSharp/ListExtensions.cs
@@ -0,0 +1,11 @@
+  public static class ListExtensions
+    {
+        public static List<List<T>> ChunkBy<T>(this List<T> source, int chunkSize)
+        {
+            return source
+                .Select((x, i) => new { Index = i, Value = x })
+                .GroupBy(x => x.Index / chunkSize)
+                .Select(x => x.Select(v => v.Value).ToList())
+                .ToList();
+        }
+    }
