540f48debd25d86086a8dd743b675b212a2afa27
Changhui Xu <changhuixu@hotmail.com>
2022-10-24 14:30:19 -0500

    git delete tags

--- bash/git.sh
+++ bash/git.sh
@@ -7,3 +7,15 @@ git push origin --delete `git branch -r | awk -F/ '/\/parcust-/{print $2 "/" $3}
 
 git branch -r | awk -F/ '/\/feature/{print $2 "/" $3}'
 git push origin --delete `git branch -r | awk -F/ '/\/feature/{print $2 "/" $3}'`
+
+###################################################################
+
+# Delete all local tags and get the list of remote tags:
+git tag -l | xargs git tag -d
+git fetch
+
+# Remove all remote tags
+git tag -l | xargs -n 1 git push --delete origin
+
+# Clean up local tags
+git tag -l | xargs git tag -d
